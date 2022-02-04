34386fd88d63051520160349c93d16cf668c4e00
DELL <DELL@192.168.2.6>
2022-02-04 10:58:14 +0530

    DataTypes Example1 added into the project 04-02-2022

---
+++ 04-02-2022/src/com/DataTypes/Example1.java
@@ -0,0 +1,19 @@
+package com.DataTypes;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		/**
+		 * write a java program to print customer information(id,name and address)
+		 */
+		
+		int customerId=7482;
+		String customerName="sanvitha";
+		String customerAddress="Nizamabad";
+		System.out.println(customerId);
+		System.out.println(customerName);
+		System.out.println(customerAddress);
+
+	}
+
+}
